8b1ceb83a0557f95e20ade6578d83881e661eebe
J-S-M-Code <joacosuilar@gmail.com>
2026-09-21 18:49:40 -0300

    refactor: initial architecture setup, standalone migration and base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle, IonBadge } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { 
+  homeOutline, gridOutline, mapOutline, layersOutline, 
+  notificationsOutline, clipboardOutline, leafOutline, 
+  pawOutline, settingsOutline, peopleOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle, IonBadge],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({
+      homeOutline, gridOutline, mapOutline, layersOutline, 
+      notificationsOutline, clipboardOutline, leafOutline, 
+      pawOutline, settingsOutline, peopleOutline
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonIcon, IonBadge, IonAvatar, IonButton } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { notificationsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonIcon, IonBadge, IonAvatar, IonButton],
 })
 export class HomePage {
-
-  constructor() {}
-
+  constructor() {
+    addIcons({ notificationsOutline });
+  }
 }
